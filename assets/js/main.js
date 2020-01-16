2440123a1ac0be64177d6906ac37d62973376482
bashvtini <bashvtini@gmail.com>
2020-01-16 20:37:04 +0100

    GB Construction

---
+++ assets/js/main.js
@@ -0,0 +1,60 @@
+const services = document.querySelector("#services ul");
+
+const Services = () => {
+  const data = [
+    {
+      icon: ["fas", "fa-fire"],
+      text: "Gas Central Heating Installation and Servicing"
+    },
+    {
+      icon: ["fas", "fa-bath"],
+      text: "Bathroom Fitters / Shower Installation/ Repair"
+    },
+    {
+      icon: ["fas", "fa-paint-roller"],
+      text: "Painting"
+    },
+    {
+      icon: ["fas", "fa-wrench"],
+      text: "Plumbing Repairs"
+    },
+    {
+      icon: ["fab", "fa-hotjar"],
+      text: "Under floor heating"
+    },
+    {
+      icon: ["fas", "fa-hot-tub"],
+      text: "Boiler installation, Repair or Servicing"
+    }
+  ];
+
+  //   <li class="p-5 bg-white rounded md:mr-2 mb-5 md:w-1/6 h-40 mx-2">
+  //         <i class="fas fa-fire text-4xl text-orange-400"></i>
+  //         <p class="text-sm pt-2">
+  //           Gas Central Heating Installation and Servicing
+  //         </p>
+  //       </li>
+
+  data.map(e => {
+    const li = document.createElement("li");
+    li.classList.add(
+      "p-5",
+      "bg-white",
+      "rounded",
+      "md:mr-2",
+      "mb-5",
+      "md:w-1/6",
+      "h-40",
+      "mx-2"
+    );
+    const i = document.createElement("i");
+    i.classList.add(e.icon[0], e.icon[1], "text-4xl", "text-orange-400");
+
+    const p = document.createElement("p");
+    p.classList.add("text-sm", "pt-2");
+    p.textContent = e.text;
+    li.append(i, p);
+    services.appendChild(li);
+  });
+};
+Services();
